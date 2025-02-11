e97ee819c3f77249f7977a11cb49f7a2879c57d6
unknown <ulugbekgozixonov@gmail.com>
2025-02-11 18:23:33 +0500

    change ddb to postgresql

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "postgresql://postgres:root_123@localhost:5432/todos"
-DATABASE_URL = "sqlite:///./todos.db"
+DATABASE_URL = "postgresql://todo_user:3Mu9aiAs4uMv5sYfHMbAhSds3wlwqd3e@dpg-culkrft2ng1s73atgm1g-a.oregon-postgres.render.com/todos_3epo"
+# DATABASE_URL = "sqlite:///./todos.db"
 engine = create_engine(url=DATABASE_URL)
 Base = declarative_base()
 
