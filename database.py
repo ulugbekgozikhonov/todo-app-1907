1db1d8e7b9d7c4a37e5fbb0d3846a9215c3abbb4
unknown <ulugbekgozixonov@gmail.com>
2025-02-11 18:03:59 +0500

    change db to sqlite

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root_123@localhost:5432/todos"
+# DATABASE_URL = "postgresql://postgres:root_123@localhost:5432/todos"
+DATABASE_URL = "sqlite:///./todos.db"
 engine = create_engine(url=DATABASE_URL)
 Base = declarative_base()
 
